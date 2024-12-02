ad48dda76d50794f21610c1303d0b6c55cf32a6a
abderrahmane140 <absarabdo4@gmail.com>
2024-12-02 19:18:35 +0100

    setup Express server and MongoDB connection

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const { config } = require('dotenv');
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("conncet to db & listen to port", process.env.PORT);
+    })
+}).catch((err)=>{
+    console.log(err);
+})
