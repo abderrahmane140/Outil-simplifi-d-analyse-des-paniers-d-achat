7a7c2bf6bfe553bf54fd3caaf2e800c133e74c3c
abderrahmane140 <absarabdo4@gmail.com>
2024-12-09 23:23:09 +0100

    add cros to allow request from my frontend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,25 @@
 require('dotenv').config()
-const { config } = require('dotenv');
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 const productRouter = require('./routes/product')
+const analyticsRouter = require('./routes/analytics')
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+    cors({
+      origin: process.env.URL_API_FRONTEND, // Only allow this frontend
+    })
+  );
+  
 
 //router
 app.use('/products',productRouter)
+app.use('/analytics',analyticsRouter)
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     app.listen(process.env.PORT, () => {
